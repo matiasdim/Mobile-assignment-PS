f174e912e1bad9eaf9f09b285573ab3f682dfcbd
Matías Gil <M.Gil@cvent.com>
2020-10-06 12:21:57 -0500

    Configuration to make the app not to use Storyboard. Custom initial view is used instead

--- MobileAssignmentPS/MobileAssignmentPS/ControllersAndViews/HomeViewController.swift
+++ MobileAssignmentPS/MobileAssignmentPS/ControllersAndViews/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  MobileAssignmentPS
 //
 //  Created by Matías Gil Echavarría on 10/6/20.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
 }
-
